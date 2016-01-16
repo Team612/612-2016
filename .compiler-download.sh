bdce3deb83d9f47265a7a2d212cdaf64e421ffca
Benjamin Ward <ward.programm3r@gmail.com>
2016-01-15 22:36:16 -0500

    Change to appending deb to end of sources.list

--- .compiler-download.sh
+++ .compiler-download.sh
@@ -4,7 +4,7 @@
 dpkg-query -s frc-toolchain
 if [ $? = 1 ]; then
 	#sudo apt-add-repository -y ppa:wpilib/toolchain 
-	cat toolchain_debs.txt >> /etc/apt/sources.list.d/wpilib-toolchain.list
+	cat toolchain_debs.txt >> /etc/apt/sources.list
 	sudo apt-get update -y
 	sudo apt-get install -y frc-toolchain
 fi
