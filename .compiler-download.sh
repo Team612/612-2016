4f581e87ae1a093fbd684a2a1ca51dedf877b78c
Benjamin Ward <ward.programm3r@gmail.com>
2016-01-15 22:40:22 -0500

    Attempt to force trusty build env and revert other changes

--- .compiler-download.sh
+++ .compiler-download.sh
@@ -3,8 +3,7 @@
 # Will download required packages for building
 dpkg-query -s frc-toolchain
 if [ $? = 1 ]; then
-	#sudo apt-add-repository -y ppa:wpilib/toolchain 
-	cat toolchain_debs.txt >> /etc/apt/sources.list
+	sudo apt-add-repository -y ppa:wpilib/toolchain 
 	sudo apt-get update -y
 	sudo apt-get install -y frc-toolchain
 fi
