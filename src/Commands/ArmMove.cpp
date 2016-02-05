41654537e1f8d75b7d863d23378541a8674f8cdf
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2016-02-05 12:21:02 -0500

    Arm subsystem and ArmMove command

---
+++ src/Commands/ArmMove.cpp
@@ -0,0 +1,40 @@
+#include "ArmMove.h"
+
+ArmMove::ArmMove()
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+	Requires(Robot::arm.get());
+	gj = Robot::oi->getGunner();
+}
+
+// Called just before this Command runs the first time
+void ArmMove::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ArmMove::Execute()
+{
+	RobotMap::armAdjust->Set(gj->GetY()); //The arm moves according to the value of the gunner's Y axis
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool ArmMove::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void ArmMove::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ArmMove::Interrupted()
+{
+
+}
---
+++ src/Commands/ArmMove.h
@@ -0,0 +1,23 @@
+#ifndef ArmMove_H
+#define ArmMove_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+#include "Joystick.h"
+
+#include "../Subsystems/Arm.h"
+
+class ArmMove: public CommandBase
+{
+private:
+	std::shared_ptr<Joystick> gj;
+public:
+	ArmMove();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+};
+
+#endif
