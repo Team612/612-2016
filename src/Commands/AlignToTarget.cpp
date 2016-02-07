937e894d524e51cc751fa103a5591fbcd5d06683
Zach Perkins <Thezperk@gmail.com>
2016-02-06 22:28:33 -0500

    Added AlignToTarget command
    - Chooses which target to align to based on aspect ratio most closely
    matching game target
    - Rotates using a PID controller to center on target (not fully
    implemented)
    - Does NOT crash if it loses a target
    - PLANNED: Add an algorithm that tilts the launcher accurately
    - PLANNED: Add a timeout so that it does not search too long for a
    target

--- src/Commands/AlignToTarget.cpp
+++ src/Commands/AlignToTarget.cpp
@@ -1,8 +1,8 @@
 #include "AlignToTarget.h"
 
-AlignToTarget::AlignToTarget()
+AlignToTarget::AlignToTarget() : PIDCommand("AlignToTarget", 1, 0, 0)
 {
-
+	Requires(CommandBase::vision.get());
 }
 
 // Called just before this Command runs the first time
@@ -14,24 +14,89 @@ void AlignToTarget::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void AlignToTarget::Execute()
 {
+	//Populate VisionTarget vector
 	CommandBase::vision->PullValues();
+
+	//Only if we need to FIND a target
+	//TODO: Add timeout so it doesn't keep trying forever
+	if (!hasTarget)
+	{
+		std::shared_ptr<NetworkTable> table = CommandBase::vision->GetRawTable();
+
+		llvm::ArrayRef<double> arr;
+
+		//See Vision subsystem for info on why we have to check for this and why we don't look
+		//at 1-8
+		if (table->GetNumberArray("BOUNDING_COORDINATES", arr).size() > 8)
+		{
+			FindTarget();
+		}
+	}
+	else
+	{
+		if (!GetPIDController()->IsEnabled())
+		{
+			GetPIDController()->SetSetpoint(SCREEN_CENTER_X); //Point we're trying to get to
+			GetPIDController()->SetAbsoluteTolerance(10); //Within 10 pixels of the actual box
+			PIDUserDisabled = false;
+			GetPIDController()->Enable();
+		}
+	}
+
+
+}
+
+void AlignToTarget::FindTarget()
+{
+	//Only executes if RoboRealm has actually detected targets
+	if (CommandBase::vision->GetTargetAmount() > 0) {
+		//Uses the FindClosestAspect static method to find the target with the closest aspect ratio to that of the real target
+		currentTarget = VisionTarget::FindClosestAspect(TARGET_ASPECT, CommandBase::vision->GetAllTargets());
+		hasTarget = true;
+	}
+}
+
+double AlignToTarget::CalculateDistance() {
+	//TODO: this
+}
+
+
+//Inherited from PID Command, returns the input from the vision targets
+double AlignToTarget::ReturnPIDInput() {
+	//Makes sure that the target still exists, if not, it goes bye bye
+	if (!CommandBase::vision->TargetExists(currentTarget->GetID()))
+	{
+		hasTarget = false;
+		PIDUserDisabled = true;
+		currentTarget = NULL;
+		GetPIDController()->Disable();
+	}
+
+
+	return currentTarget->GetCenter().x;
+}
+
+void AlignToTarget::UsePIDOutput(double output) {
+	//TODO: Make this rotate the bot
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool AlignToTarget::IsFinished()
 {
-	return false;
+	return !GetPIDController()->IsEnabled() && !PIDUserDisabled;
+	//Only end if the PID controller is done AND it HASN'T been user disabled (meaning it succeeded)
 }
 
 // Called once after isFinished returns true
 void AlignToTarget::End()
 {
-
+	//TODO: Calculate launch angle and move the launcher here
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void AlignToTarget::Interrupted()
 {
-
+	GetPIDController()->Disable();
+	//Can't decide if I need to do anything else here
 }
--- src/Commands/AlignToTarget.h
+++ src/Commands/AlignToTarget.h
@@ -4,7 +4,7 @@
 #include "../CommandBase.h"
 #include "WPILib.h"
 
-class AlignToTarget: public CommandBase
+class AlignToTarget: public PIDCommand //PID for rotation
 {
 public:
 	AlignToTarget();
@@ -13,6 +13,20 @@ public:
 	bool IsFinished();
 	void End();
 	void Interrupted();
+
+	double ReturnPIDInput();
+	void UsePIDOutput(double output);
+
+private:
+	bool hasTarget = false; //If we are tracking a target
+	bool PIDUserDisabled = true; //If the PID is disabled because it succeeded or because the code forced it off
+	std::shared_ptr<VisionTarget> currentTarget; //Currently tracked target (=NULL if no target)
+
+	void FindTarget(); //Tries to fill the CurrentTarget variable
+	double CalculateDistance();
+	const double TARGET_ASPECT = 1.66/1.17; //Aspect ratio of game-target (it's 1'8" x 1'2")
+	const int SCREEN_CENTER_X = 320; //Center of the screen. This is the point to which we will rotate
+								     //using PID.
 };
 
 #endif
