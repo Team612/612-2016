7113be9ec5b26167999c7172fbbc4189dc6ed289
Dominic Philip <domi.a.philip@gmail.com>
2016-02-06 19:37:33 -0500

    Cosmetic changes and added 'default' case to switch statement.

--- src/Commands/BreachAuto.cpp
+++ src/Commands/BreachAuto.cpp
@@ -2,18 +2,14 @@
 
 BreachAuto::BreachAuto()
 {
-	// Use Requires() here to declare subsystem dependencies
-	// eg. Requires(chassis);
 	Requires(Robot::drivetrain.get());
 }
 
-// Called just before this Command runs the first time
 void BreachAuto::Initialize()
 {
 
 }
 
-// Called repeatedly when this Command is scheduled to run
 void BreachAuto::Execute()
 {
 	switch (defense)
@@ -38,23 +34,21 @@ void BreachAuto::Execute()
 			break;
 		case Robot::Defense::NONE:
 			break;
+		default:
+			break;
 	}
 }
 
-// Make this return true when this Command no longer needs to run execute()
 bool BreachAuto::IsFinished()
 {
 	return true;
 }
 
-// Called once after isFinished returns true
 void BreachAuto::End()
 {
 
 }
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
 void BreachAuto::Interrupted()
 {
 
