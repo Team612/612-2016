eaf47bb0fa164b429fbdd7fe113af385562e9e1a
Zach Perkins <Thezperk@gmail.com>
2016-02-06 22:23:12 -0500

    Update VisionTarget
    - Added comparator method for aspect ratio (for use in AlignToTarget)
    - Removed PIDSource inheritance (Now implemented more cleanly in
    AlignToTarget)

--- VisionTarget.cpp
+++ VisionTarget.cpp
@@ -1,28 +1,14 @@
 #include "VisionTarget.h"
 
-VisionTarget::VisionTarget(std::vector<double> initPoints)
+VisionTarget::VisionTarget(std::vector<int> initPoints, int id)
 {
-	ul.x = (int)initPoints[0];
-	ul.y = (int)initPoints[1];
-
-	ur.x = (int)initPoints[2];
-	ur.y = (int)initPoints[3];
-
-	ll.x = (int)initPoints[4];
-	ll.y = (int)initPoints[5];
-
-	lr.x = (int)initPoints[6];
-	lr.y = (int)initPoints[7];
+	Set(initPoints);
+	this->id = id;
 }
 
 VisionTarget::~VisionTarget()
 {
 	// TODO Auto-generated destructor stub
-}
-
-double VisionTarget::PIDGet()
-{
-	return GetCenter().x; //shut up, your mom is an arbitrary value
 }
 
 Point VisionTarget::GetUL()
@@ -63,4 +49,56 @@ int VisionTarget::GetWidth()
 int VisionTarget::GetHeight()
 {
 	return ll.y - ul.y;
+}
+
+int VisionTarget::GetID()
+{
+	return id;
+}
+
+void VisionTarget::Set(std::vector<int> setPoints) {
+	ul.x = (int)setPoints[0];
+	ul.y = (int)setPoints[1];
+
+	ur.x = (int)setPoints[2];
+	ur.y = (int)setPoints[3];
+
+	ll.x = (int)setPoints[4];
+	ll.y = (int)setPoints[5];
+
+	lr.x = (int)setPoints[6];
+	lr.y = (int)setPoints[7];
+}
+
+
+double VisionTarget::GetAspectRatio() {
+	return GetWidth() / GetHeight();
+}
+
+//Basic print method for debugging
+void VisionTarget::Print() {
+	printf("ID: %u \n", id);
+	printf("UL (%u, %u) \n", ul.x, ul.y);
+	printf("UR (%u, %u) \n", ur.x, ur.y);
+	printf("LL (%u, %u) \n", ll.x, ll.y);
+	printf("LR (%u, %u) \n", lr.x, lr.y);
+
+}
+
+//Finds a VisionTarget in a list of VisionTargets that most closely matches the given aspect
+//ratio
+std::shared_ptr<VisionTarget> VisionTarget::FindClosestAspect(double aspect,
+		std::vector<std::shared_ptr<VisionTarget>> targets)
+{
+	int closest = 0; //i.e. closest to the given number
+	for (int x = 1; x < targets.size(); x++)
+	{
+		if (abs(aspect - targets[x]->GetAspectRatio()) <
+				abs(aspect - targets[closest]->GetAspectRatio()))
+		{
+			closest = x;
+		}
+	}
+
+	return targets[closest];
 }
--- VisionTarget.h
+++ VisionTarget.h
@@ -2,30 +2,43 @@
 #define VISIONTARGET_H_
 
 #include "WPILib.h"
+#include <cmath>
 
 //Represents basically a bounding box (with a few perks)
-class VisionTarget : PIDSource
+class VisionTarget
 {
 public:
-	VisionTarget(std::vector<double> initPoints);
+	VisionTarget(std::vector<int> initPoints, int id); //See "Set()"
 	virtual ~VisionTarget();
-	double PIDGet(); //Required for PIDSource
 
-	Point GetUL();
-	Point GetLL();
-	Point GetUR();
-	Point GetLR();
+	Point GetUL(); //Upper-Left
+	Point GetLL(); //Lower-Left
+	Point GetUR(); //Upper-Right
+	Point GetLR(); //Lower-Right
 
 	Point GetCenter();
 
 	int GetWidth();
 	int GetHeight();
 
+	double GetAspectRatio(); //width/height
+
+	int GetID(); //The tracking ID set by RoboRealm to distinguish one object as it moves frame to frame
+
+	void Set(std::vector<int> setPoints); //Sets the points in a size 8 vector in the following format {ulx, uly, urx, ury, llx, lly, lrx, lry}
+	//(This is hour RoboRealm formats points sent from bounding boxes)
+
+	void Print();
+
+	static std::shared_ptr<VisionTarget> FindClosestAspect(double aspect, std::vector<std::shared_ptr<VisionTarget>> targets);
+
 private:
 	Point ul; //Upper-Left point
 	Point ll;
 	Point ur;
 	Point lr;
+
+	int id; //The tracking ID as given by RoboRealm
 };
 
 #endif /* VISIONTARGET_H_ */
