08d4cb6b805b64533666b4c8553c47e704e7bf6f
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2016-02-23 16:38:28 -0500

    Fixed compiler errors. Added code that should reset the arm before autonomous.

--- src/Commands/ArmToPosition.cpp
+++ src/Commands/ArmToPosition.cpp
@@ -1,4 +1,5 @@
 #include "ArmToPosition.h"
+#include "Robot.h"
 #include <cmath>
 
 /*
@@ -6,6 +7,8 @@
  * to this command. Thanks Ben!
  * It may be a good idea to make sure to set the arm to
  * neutral (0 degrees) before this command is called.
+ *
+ * Note that this is completely temporary!
  */
 
 ArmToPosition::ArmToPosition(float position)
@@ -26,7 +29,7 @@ void ArmToPosition::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void ArmToPosition::Execute()
 {
-	if(abs(Robot::arm.get()->GetDegrees() - position) < THREASHHOLD)
+	if(abs(Robot::arm.get()->GetDegrees() - position) < THRESHOLD)
 		power /= SCALE;
 
 	/*
@@ -42,31 +45,29 @@ void ArmToPosition::Execute()
 	 */
 
 	if(Robot::arm.get()->GetDegrees() < position && !(abs(Robot::arm.get()->GetDegrees() - position) < (position + CLOSE_ENOUGH) && abs(Robot::arm.get()->GetDegrees() - position) > (position - CLOSE_ENOUGH)))
-		Robot::arm.get()->adjust->Set(power);
+		RobotMap::armAdjust->Set(power);
 	else if(Robot::arm.get()->GetDegrees() > position && !(abs(Robot::arm.get()->GetDegrees() - position) < (position + CLOSE_ENOUGH) && abs(Robot::arm.get()->GetDegrees() - position) > (position - CLOSE_ENOUGH)))
-		Robot::arm.get()->adjust->Set(power * -1);
-	else if (abs(Robot::arm.get()->GetDegrees() - position) < (position + CLOSE_ENOUGH) && abs(Robot::arm.get()->GetDegrees() - position) > (position - CLOSE_ENOUGH))
-	{
-		Robot::arm.get()->adjust->Set(0);
-		std::printf("Arm set to position")
-	}
+		RobotMap::armAdjust->Set(power * -1);
+
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool ArmToPosition::IsFinished()
 {
-	return false;
+	return abs(Robot::arm.get()->GetDegrees() - position) < (position + CLOSE_ENOUGH) && abs(Robot::arm.get()->GetDegrees() - position) > (position - CLOSE_ENOUGH);
 }
 
 // Called once after isFinished returns true
 void ArmToPosition::End()
 {
-
+	RobotMap::armAdjust->Set(0);
+	std::printf("Arm set to position\n");
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void ArmToPosition::Interrupted()
 {
-
+	RobotMap::armAdjust->Set(0);
+	std::printf("ERROR: Arm movement interrupted!\n");
 }
